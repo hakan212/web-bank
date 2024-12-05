ad8d4f3e617e4f0948a148a0942846c08bf61d72
Hakan <hakan.bas2@outlook.com>
2024-12-05 11:49:09 +0000

    adding sample project from Ktor using gradle

---
+++ src/test/kotlin/webbank/HttpClientRequest.kt
@@ -0,0 +1,13 @@
+package webbank
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class HttpClientRequest {
+
+    @Test
+    fun requestOK() {
+        assertEquals(1, 100)
+    }
+}
